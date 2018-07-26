f78735a2da302a14b3ee58a011bb7996e374aca9
Edin Coralic <edin.coralic@cartargetgroup.com>
2018-07-26 09:49:46 +0200

    Endpoint now have nicer print-out.

---
+++ code/index.php
@@ -0,0 +1,7 @@
+
+<h2>Hello</h2>
+
+<div>HOSTNAME: <b><?= $_SERVER['HOSTNAME'] ?></b></div>
+<div>HTTP_HOST: <b><?= $_SERVER['HTTP_HOST'] ?></b></div>
+<div>SERVER_ADDR: <b><?= $_SERVER['SERVER_ADDR'] ?></b></div>
+<div>COOKIE: <b><?= json_encode($_COOKIE) ?></b></div>
